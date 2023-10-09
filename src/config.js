c1468f770c88bc0266cd27e255ba21c314208896
siddhesh-hit <siddhesh@handsintechnology.com>
2023-10-09 14:57:22 +0530

    change url

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export const API = {
   mysqllocalurl: "https://api.strategytool.io",
   strapiurl: "https://admin.strategytool.io",
   mysqlurl: "https://api.strategytool.io",
-  localhost: "http://localhost:5000/api",
+  // localhost: "http://localhost:5000/api",
+  localhost: "https://strategy-back.onrender.com/api",
   basic: "http://localhost:5000",
 };
